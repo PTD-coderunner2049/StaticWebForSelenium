b3e03015c0d59f61130dd1ac5f00541d073fb58f
iloveMlord <ilovemilord.op@gmail.com>
2025-10-29 23:22:03 +0700

    remove bk proj, pull readme out.

--- StaticWebForSelenium_bk/wwwroot/js/itemloadup.js
+++
@@ -1,4 +0,0 @@
-const localStorageKey = 'allProductsData';
-const productsJsonString = JSON.stringify(products);
-localStorage.setItem(localStorageKey, productsJsonString);
-console.log("Products successfully loaded into localStorage!");
